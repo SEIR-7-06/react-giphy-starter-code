0d9b9ae237dbc9977dc9606aa40b5845bd8c2ba0
msolorio <msolorio.github@gmail.com>
2021-08-30 13:56:19 -0700

    Adds functionality for search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,42 @@
-function Search() {
-  return (
-    <div>Search</div>
-  )
+import React from 'react';
+
+// API key = 8tb9DmbAnGgTzCwgOYVhRtIlcnELsQgx
+
+class Search extends React.Component {
+  state = {
+    inputVal: 'iguana'
+  }
+
+  // Update inputVal to be whatever the user typed
+  handleChange = (event) => {
+    // event.target.value = the key the user pressed on the their keyboard
+    this.setState({ inputVal: event.target.value });
+  }
+
+  handleFormSubmit = (event) => {
+    // prevent default behavior of submitting a form
+    // we don't want the page to refresh
+    event.preventDefault();
+
+    console.log('form was submitted!');
+
+    this.props.fetchGifs();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <input 
+            type="text" 
+            value={this.state.inputVal} 
+            onChange={this.handleChange}  
+          />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Search;
--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -3,11 +3,21 @@ import Search from './Search';
 import Results from './Results';
 
 class SearchContainer extends React.Component {
+  state = {
+    searchResults: []
+  }
+
+  // Create a function that is in charge of updating state
+  // This function needs to know what gifs to search for
+  fetchGifs = (searchTerm) => {
+    console.log('fetching the gifs');
+    // Todo: go fetch all of the gigs
+  }
+
   render() {
     return (
       <div>
-        Search Container
-        <Search />
+        <Search fetchGifs={this.fetchGifs} />
         <Results />
       </div>
     )
